Fixed test model accuracy calculation

load_model.py
@@ -4,6 +4,8 @@ from collections import OrderedDict
 import torch
 import sys,os
 
+eligible_models = ['vgg11','vgg11_bn','vgg13','vgg13_bn','vgg16','vgg16_bn','vgg19','vgg19_bn']
+
 def select_pretrained_model(model_name,hidden_units,no_output_categories):
     '''
     Inputs: 
@@ -18,18 +20,41 @@ def select_pretrained_model(model_name,hidden_units,no_output_categories):
     
     Classifier to be trained is replaced to work with 220x220 PIL images.
     '''
-    pretrained_model = getattr(models,model_name)(pretrained=True)
-    for param in pretrained_model.parameters():
-        param.requires_grad = False
-
-    if pretrained_model.classifier.in_features < 18432:
+    try:
+        pretrained_model = getattr(models,model_name)(pretrained=True)
+        for param in pretrained_model.parameters():
+            param.requires_grad = False
+    
+    except:
         os.system('clear')
-        print("Selected model classifier does not have enough input features. Please select another model.")
+        print("Incompatible Model. Please select another model. The following models have been tested and work with this program...")
+        for m in eligible_models:
+            print(m)
+        sys.exit()        
+
+    try:
+        pretrained_model.classifier
+    except:
+        os.system('clear')
+        print("Selected model does not have a classifier. Please select another model. The following models have been tested and work with this program...")
+        for m in eligible_models:
+            print(m)
         sys.exit()
+
+    try:    
+        if pretrained_model.classifier.in_features < 18432:
+            os.system('clear')
+            print("Selected model classifier does not have enough input features. Please select another model. The following models have been tested and work with this program...")
+        for m in eligible_models:
+            print(m)
+            sys.exit()
+    except:
+        pass
 
     classifier = nn.Sequential(OrderedDict([
                                 ('fc1', nn.Linear(18432,hidden_units)),
                                 ('relu', nn.ReLU()),
+                                # ('Dropout', nn.Dropout(0.2)), # Not added yet - to test
                                 ('fc2', nn.Linear(hidden_units,no_output_categories)),
                                 ('output', nn.LogSoftmax(dim=1))
                                 ]))

train.py
@@ -3,7 +3,6 @@ import data_processing,load_model,argparse
 from torch import optim,nn
 from workspace_utils import active_session # Provided by Udacity - Purpose: Keep GPU session active 
 import torch
-import matplotlib.pyplot as plt
 
 def train_model(model,train_dataloader,valid_dataloader,test_dataloader,epochs,device,lr,print_every):
     '''
@@ -94,14 +93,6 @@ def train_model(model,train_dataloader,valid_dataloader,test_dataloader,epochs,d
             print("=======================================")
             print(f'Total Elapsed Time for Epoch {e+1}: {epoch_time:.3f} seconds.')
             print("=======================================\n")
-        
-        # Plotting training data
-        plt.figure()
-        plt.title("Training Summary")
-        plt.plot(training_losses,label='Training Loss')
-        plt.plot(validation_losses,label='Validation Loss')
-        plt.legend()
-        plt.show()
 
         print("Evaluating Model on Testing Data...\n")
         test_accuracy = 0
@@ -115,7 +106,7 @@ def train_model(model,train_dataloader,valid_dataloader,test_dataloader,epochs,d
             accuracy = matches.mean()
             test_accuracy += accuracy
             model.train()
-        print(f'Model Test Accuracy: {test_accuracy*100:.2f}%\n')
+        print(f'Model Test Accuracy: {test_accuracy/len(test_dataloader)*100:.2f}%\n')
 
 def save_model(trained_model,hidden_units,output_units,dest_dir,model_arch,class_to_idx):
     model_checkpoint = {'model_arch':model_arch, 
@@ -127,8 +118,10 @@ def save_model(trained_model,hidden_units,output_units,dest_dir,model_arch,class
                     }
     if dest_dir:
         torch.save(model_checkpoint,dest_dir+"/"+model_arch+"_checkpoint.pth")
+        print(f"{model_arch} successfully saved to {dest_dir}")
     else:
         torch.save(model_checkpoint,model_arch+"_checkpoint.pth")
+        print(f"{model_arch} successfully saved to current directory as {model_arch}_checkpoint.pth")
 
 def main():
     parser = argparse.ArgumentParser()